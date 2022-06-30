088466a1c721be12cdbd5263f2ae5f2b764c4497
unknown <neeladri22@gmail.com>
2022-06-30 13:19:21 +0530

    Added TemperaturConversion program file

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -8,45 +8,30 @@ namespace LogicalPrograms
     {
         static void Main(string[] args)
         {
-            int day = dayofweek(05, 10, 2000);
+            double fahre;
 
-            switch (day)
+            Console.WriteLine("Press 0 to Convert Celsius to Fahrenheit Press 1 for Fahrenheit to Celsius ");
+            int option = int.Parse(Console.ReadLine());
+            switch (option)
             {
                 case 0:
-                    Console.WriteLine("Sunday");
+                    Console.WriteLine("Enter Celcius:");
+                    double celsius = double.Parse(Console.ReadLine());
+                    fahre = (celsius * 9 / 5) + 32;
+                    Console.WriteLine("Fahrenheit: " + fahre);
                     break;
+
                 case 1:
-                    Console.WriteLine("Monday");
+                    Console.WriteLine("Enter Fahrenheit:");
+                    double fahrenheit = double.Parse(Console.ReadLine());
+                    celsius = (fahrenheit - 32) * 5 / 9;
+                    Console.WriteLine("Celcius: " + celsius);
                     break;
-                case 3:
-                    Console.WriteLine("Tuesday");
-                    break;
-                case 4:
-                    Console.WriteLine("Wednesday");
-                    break;
-                case 5:
-                    Console.WriteLine("Thursday");
-                    break;
-                case 6:
-                    Console.WriteLine("Friday");
-                    break;
-                case 7:
-                    Console.WriteLine("Saturday");
+                default:
                     break;
 
             }
 
-            Console.Write(day);
-
-        }
-
-        private static int dayofweek(int d, int m, int y)
-        {
-            int[] t = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
-
-            y -= (m < 3) ? 1 : 0;
-
-            return (y + y / 4 - y / 100 + y / 400 + t[m - 1] + d) % 7;
-        }
+        }   
     }
 }
