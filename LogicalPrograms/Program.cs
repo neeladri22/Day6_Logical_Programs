ba7708f1f8f3822036580c7cf54fff9604be8f1a
unknown <neeladri22@gmail.com>
2022-06-30 13:26:10 +0530

    Added VendingMachine program file

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -8,30 +8,35 @@ namespace LogicalPrograms
     {
         static void Main(string[] args)
         {
-            double fahre;
+            Console.WriteLine("Enter Amount for Change needed");
+            int amount = int.Parse(Console.ReadLine());
+            countCurrency(amount);
+        }
+        public static void countCurrency(int amount)
+        {
+            int[] notes = new int[] { 1000, 500, 200, 100, 50, 10, 5, 2, 1 };
+            int[] noteCounter = new int[9];
 
-            Console.WriteLine("Press 0 to Convert Celsius to Fahrenheit Press 1 for Fahrenheit to Celsius ");
-            int option = int.Parse(Console.ReadLine());
-            switch (option)
+            // count notes 
+            for (int i = 0; i < 9; i++)
             {
-                case 0:
-                    Console.WriteLine("Enter Celcius:");
-                    double celsius = double.Parse(Console.ReadLine());
-                    fahre = (celsius * 9 / 5) + 32;
-                    Console.WriteLine("Fahrenheit: " + fahre);
-                    break;
-
-                case 1:
-                    Console.WriteLine("Enter Fahrenheit:");
-                    double fahrenheit = double.Parse(Console.ReadLine());
-                    celsius = (fahrenheit - 32) * 5 / 9;
-                    Console.WriteLine("Celcius: " + celsius);
-                    break;
-                default:
-                    break;
-
+                if (amount >= notes[i])
+                {
+                    noteCounter[i] = amount / notes[i];
+                    amount = amount - noteCounter[i] * notes[i];
+                }
             }
 
-        }   
+            // Print notes
+            Console.WriteLine("Currency Count -");
+            for (int i = 0; i < 9; i++)
+            {
+                if (noteCounter[i] != 0)
+                {
+                    Console.WriteLine(notes[i] + " : "
+                        + noteCounter[i]);
+                }
+            }
+        }
     }
 }
