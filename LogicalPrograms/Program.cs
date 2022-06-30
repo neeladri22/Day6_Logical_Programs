6f532f75635d48749d8c12efbe2b506b747a94c6
unknown <neeladri22@gmail.com>
2022-06-30 13:13:48 +0530

    Added Day of Week program file

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -8,20 +8,45 @@ namespace LogicalPrograms
     {
         static void Main(string[] args)
         {
-            couponNumbers();
-        }
-        public static void couponNumbers()
-        {
-            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-            var stringChars = new char[6];
-            var random = new Random();
+            int day = dayofweek(05, 10, 2000);
 
-            for (int i = 0; i < stringChars.Length; i++)
+            switch (day)
             {
-                stringChars[i] = chars[random.Next(chars.Length)];
-            }
-            Console.WriteLine(stringChars);
+                case 0:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 3:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 5:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 6:
+                    Console.WriteLine("Friday");
+                    break;
+                case 7:
+                    Console.WriteLine("Saturday");
+                    break;
 
+            }
+
+            Console.Write(day);
+
+        }
+
+        private static int dayofweek(int d, int m, int y)
+        {
+            int[] t = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
+
+            y -= (m < 3) ? 1 : 0;
+
+            return (y + y / 4 - y / 100 + y / 400 + t[m - 1] + d) % 7;
         }
     }
 }
