12390c6a5d0c99bcde8b97788c71add8e23b1aa8
unknown <neeladri22@gmail.com>
2022-06-30 12:42:50 +0530

    Added the fabonacci program file

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -10,7 +10,22 @@ namespace LogicalPrograms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to logical program");
+            FibonacciSeries();
+        }
+        public static void FibonacciSeries()
+        {
+            int n1 = 0, n2 = 1, n3, num = 5;
+            Console.WriteLine(n1 + " " + n2);
+
+            for (int i = 2; i < num; ++i)
+            {
+                n3 = n1 + n2;
+                Console.WriteLine("print the fibonnaci number :" + n3);
+                n1 = n2;
+                n2 = n3;
+            }
+
+
         }
     }
 }
